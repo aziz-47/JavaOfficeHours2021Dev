bd04278122ef9a099795d19767ea952f3f13aade
Aziz <azizekeathn@gmail.com>
2021-03-16 18:35:20 +0200

    office hours

---
+++ src/day1/Question01.java
@@ -0,0 +1,32 @@
+package day1;
+
+import java.util.Scanner;
+
+public class Question01 {
+
+	public static void main(String[] args) {
+		/*
+		 * Kullanicidan bir karakter girmesini isteyip, o karakterin ascii degerini
+		 * yazdirin ORNEK: INPUT : # OUTPUT : girdiginiz # karakterinin ascii degeri
+		 * = 35'dir.
+		 */
+
+		
+
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Lutfen bir karakter giriniz");
+			char karakter = scan.next().charAt(0);
+
+			int karakterAscii = (int) karakter;
+
+			System.out.println("girdiginiz " + karakter
+					+ " karakterinin ascii degeri: " + karakterAscii);
+
+			System.out.println(karakter + 0);
+
+			scan.close();
+
+
+	}
+
+}
